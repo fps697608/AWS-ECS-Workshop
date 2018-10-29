b1a1aed0d59194a2e41c866950c1ce9472af71f4
fps697608 <fps697608@gmail.com>
2018-10-29 14:18:48 +0800

    Update stressCPU.php

--- src/stressCPU.php
+++ src/stressCPU.php
@@ -5,6 +5,6 @@
 		echo "CPU is burning right now.";
 	} else {
 		echo("Start burning CPU for 5 minutes...");
-		exec("stress -c 1 -t 300 > /dev/null &");
+		exec("bash -c 'stress -c 1 -t 300 > /dev/null &' ");
 	}
 ?>
