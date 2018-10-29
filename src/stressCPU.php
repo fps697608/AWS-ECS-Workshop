785241f20119c9eb68ff1398cd31b3b5ab6a0221
fps697608 <fps697608@gmail.com>
2018-10-29 16:52:53 +0800

    Update stressCPU.php (Final)

--- src/stressCPU.php
+++ src/stressCPU.php
@@ -1,10 +1,9 @@
 <?php
 	$isRunning = exec("pgrep stress");
-	
-	if($isRunning){
-		echo "CPU is burning right now.";
-	} else {
-		echo("Start burning CPU for 5 minutes...");
-		exec("bash -c 'stress -c 1 -t 300 > /dev/null &' ");
-	}
+		if($isRunning) {
+			echo "CPU is burning right now.";
+		} else {
+			echo "Start burning CPU for 5 minutes...";
+			$return = exec("nohup nice -n 10 stress -c 1 -t 360 > /dev/null &");
+		}
 ?>
